d0e68d2c93c9753e800a7e9556a2dc0e9cb5f08c
Samuel Hinton <sh@arenko.group>
2021-04-09 16:18:51 +0100

    Test for Halima

---
+++ ignoreme.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+plt.style.use("default")
+plt.rcParams["figure.figsize"] = (11, 6)
+plt.rcParams["figure.dpi"] = 200
+plt.rcParams["axes.labelsize"] = 12
+plt.rcParams["ytick.labelsize"] = 12
+plt.rcParams["xtick.labelsize"] = 12
+plt.rcParams["font.size"] = 12
+plt.rcParams["image.cmap"] = "magma"
+plt.rcParams["figure.subplot.wspace"] = 0.05
+plt.rc("text", usetex=False)
+plt.rc("font", family="sans-serif")
